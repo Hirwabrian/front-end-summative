2e79a75f0b514b90f79d3c4730c4f2930d0a671e
Hirwabrian <H.brian@alustudent.com>
2024-06-18 15:55:52 +0200

    first

--- fictional blog/script.js
+++ fictional blog/script.js
@@ -1,9 +1,93 @@
 document.addEventListener("DOMContentLoaded", () => {
+    // Add mouseover event listener to elements with class "word"
     const links = document.querySelectorAll(".word");
     links.forEach((link) => {
         link.addEventListener('mouseover', shuffleanimation);
     });
 
+    // Add mouseover event listener to elements with class "logo"
+    const logos = document.querySelectorAll(".logo");
+    logos.forEach((logo) => {
+        logo.addEventListener('mouseover', shuffleanimation);
+    });
+
+    // Add mouseover event listener to elements with class "login-btn"
+    const logins = document.querySelectorAll(".login-btn");
+    logins.forEach((login) => {
+        login.addEventListener('mouseover', shuffleanimation);
+    });
+
+    // Function to generate a random character
+    function randomchar() {
+        const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        return chars[Math.floor(Math.random() * chars.length)];
+    }
+    
+    // Function to shuffle the text content of an element
+    function shuffleanimation(event) {
+        const target = event.currentTarget;
+        if (target.dataset.animating) {
+            return;
+        }
+        target.dataset.animating = true;
+
+        const originalText = target.textContent;
+        let shuffles = 0;
+        const maxShuffles = 10;
+        const intervalDuration = 500 / maxShuffles;
+
+        // Interval to change the text content at regular intervals
+        let animationInterval = setInterval(() => {
+            if (shuffles >= maxShuffles) {
+                clearInterval(animationInterval);
+                target.textContent = originalText;
+                delete target.dataset.animating;
+            } else {
+                let shuffledText = "";
+                for (let i = 0; i < originalText.length; i++) {
+                    shuffledText += randomchar();
+                }
+                target.textContent = shuffledText;
+                shuffles++;
+            }
+        }, intervalDuration);
+    }
+    
+    // Function to generate a random position within the window
+    function getRandomPosition() {
+        var x = window.innerWidth - 100;
+        var y = window.innerHeight - 100;
+        var randomX = Math.floor(Math.random() * x);
+        var randomY = Math.floor(Math.random() * y);
+        return [randomX, randomY];
+    }
+
+    // Function to create and animate the "RAPUP" element
+    function createRapup() {
+        var rapupElement = document.createElement('div');
+        rapupElement.textContent = 'RAPUP';
+        rapupElement.classList.add('rapup');
+        
+        var [randomX, randomY] = getRandomPosition();
+        rapupElement.style.left = randomX + 'px';
+        rapupElement.style.top = randomY + 'px';
+        
+        document.body.appendChild(rapupElement);
+        
+        var duration = Math.random() * 4000 + 2000;
+        setTimeout(function() {
+            rapupElement.remove();
+        }, duration);
+    }
+
+    // Function to repeatedly create "RAPUP" elements at intervals
+    function generateRandomRapup() {
+        setInterval(createRapup, 1500);
+    }
+
+    generateRandomRapup();
+    
+    // click event to show the popup form
     const wrapper = document.querySelector('.wrapper');
     const btnpopup = document.querySelector('.login-btn');
     btnpopup.addEventListener('click', () => {
@@ -11,11 +95,21 @@ document.addEventListener("DOMContentLoaded", () => {
         switchTab('form-box');
     });
 
+    // click event to close the popup form
     const iclose = document.querySelector('.close');
     iclose.addEventListener('click', () => {
         wrapper.classList.remove('active-popup');
     });
 
+    // Function to switch tabs in the popup form
+    function switchTab(tabId) {
+        document.querySelectorAll('.tab').forEach(tab => {
+            tab.classList.remove('active');
+        });
+        document.getElementById(tabId).classList.add('active');
+    }
+
+    // handle switch if form is valid
     const form = document.getElementById('form');
     form.addEventListener('submit', (event) => {
         event.preventDefault();
@@ -25,24 +119,21 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 
+    // Handle preferences form submission
     const preferencesForm = document.getElementById('preferencesForm');
     preferencesForm.addEventListener('submit', (event) => {
         event.preventDefault();
         alert('Preferences saved!');
+        wrapper.classList.remove('active-popup');
     });
 
+    // Handle click event to hide the error modal
     const okbtn = document.getElementById('okbtn');
     okbtn.addEventListener('click', () => {
-        document.getElementById('modal').style.display = 'none';
+        document.getElementById('modal').style.visibility = 'hidden';
     });
 
-    function switchTab(tabId) {
-        document.querySelectorAll('.tab').forEach(tab => {
-            tab.classList.remove('active');
-        });
-        document.getElementById(tabId).classList.add('active');
-    }
-
+    // Function to validate the subscription form
     function validateForm() {
         const firstName = document.getElementById('fname').value;
         const lastName = document.getElementById('lname').value;
@@ -75,6 +166,7 @@ document.addEventListener("DOMContentLoaded", () => {
         return isValid;
     }
 
+    // Function to validate the password
     function validatePassword(password) {
         let minLength = 8;
         let hasNumber = /\d/.test(password);
@@ -84,84 +176,23 @@ document.addEventListener("DOMContentLoaded", () => {
         return password.length >= minLength && hasNumber && hasUpper && hasLower && hasSpecial;
     }
 
+    // Function to validate the first name
     function validatefName(name) {
         const nameRegex = /^[a-zA-Z]+$/;
         return nameRegex.test(name);
     }
 
+    // Function to validate the last name
     function validatelName(name) {
         const nameRegex = /^[a-zA-Z]+$/;
         return nameRegex.test(name);
     }
 
+    // Function to show an error message in a modal
     function showError(message) {
         const errorMessage = document.getElementById('errorM');
         const modal = document.getElementById('modal');
         errorMessage.innerHTML = message;
-        modal.style.display = 'block';
+        modal.style.visibility = 'visible';
     }
-
-    function randomchar() {
-        const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-        return chars[Math.floor(Math.random() * chars.length)];
-    }
-
-    function shuffleanimation(event) {
-        const target = event.currentTarget;
-        if (target.dataset.animating) {
-            return;
-        }
-        target.dataset.animating = true;
-
-        const originalText = target.textContent;
-        let shuffles = 0;
-        const maxShuffles = 10;
-        const intervalDuration = 500 / maxShuffles;
-
-        let animationInterval = setInterval(() => {
-            if (shuffles >= maxShuffles) {
-                clearInterval(animationInterval);
-                target.textContent = originalText;
-                delete target.dataset.animating;
-            } else {
-                let shuffledText = "";
-                for (let i = 0; i < originalText.length; i++) {
-                    shuffledText += randomchar();
-                }
-                target.textContent = shuffledText;
-                shuffles++;
-            }
-        }, intervalDuration);
-    }
-
-    function getRandomPosition() {
-        var x = window.innerWidth - 100;
-        var y = window.innerHeight - 100;
-        var randomX = Math.floor(Math.random() * x);
-        var randomY = Math.floor(Math.random() * y);
-        return [randomX, randomY];
-    }
-
-    function createRapup() {
-        var rapupElement = document.createElement('div');
-        rapupElement.textContent = 'RAPUP';
-        rapupElement.classList.add('rapup');
-        
-        var [randomX, randomY] = getRandomPosition();
-        rapupElement.style.left = randomX + 'px';
-        rapupElement.style.top = randomY + 'px';
-        
-        document.body.appendChild(rapupElement);
-        
-        var duration = Math.random() * 4000 + 2000;
-        setTimeout(function() {
-            rapupElement.remove();
-        }, duration);
-    }
-
-    function generateRandomRapup() {
-        setInterval(createRapup, 1500);
-    }
-
-    generateRandomRapup();
 });
